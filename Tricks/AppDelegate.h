6dd126607dbd9992cdae291774ed37152eadee9e
AnatolyRyavkin <toryavkin@gmail.com>
2019-08-26 11:40:18 +0300

    Initial Commit

---
+++ Tricks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tricks
+//
+//  Created by Anatoly Ryavkin on 26/08/2019.
+//  Copyright © 2019 AnatolyRyavkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tricks/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Tricks
+//
+//  Created by Anatoly Ryavkin on 26/08/2019.
+//  Copyright © 2019 AnatolyRyavkin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
